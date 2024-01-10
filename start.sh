a96719f75a9bd498b392a853dc6c06e9900878b1
Seth Erickson <sr.erickson@gmail.com>
2024-01-10 15:49:07 -0800

    update start script for testing

--- start.sh
+++ start.sh
@@ -1,16 +1,20 @@
-TOKEN_KEY='hack/server/chaparral.pem'
+# server startup script for testing
+
+TOKEN_KEY='hack/server/chaparral.pem' # will be created
 TOKEN_USERID="0"
 TOKEN_EMAIL="nobody@nothing.never"
 TOKEN_NAME="test user"
 TOKEN_ROLES="chaparral:member"
 
+# generate signing key
 openssl genrsa -out $TOKEN_KEY
 
+# generate signed client bearer token
 echo "client bearer token:"
 go run ./cmd/chaptoken -key $TOKEN_KEY -id $TOKEN_USERID -email $TOKEN_EMAIL -name $NAME -roles $TOKEN_ROLES -exp 1
 
-echo "starting server..."
 # start a local server
+echo "starting server..."
 CHAPARRAL_AUTH_PEM=$TOKEN_KEY \
 CHAPARRAL_BACKEND=file://hack/server \
 CHAPARRAL_DB=hack/server/chaparral.sqlite3 \
