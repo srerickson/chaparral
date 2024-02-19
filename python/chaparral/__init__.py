c6e9c45e966c2f9e0fb92c8bc01ff22e675e3f8c
Seth Erickson <sr.erickson@gmail.com>
2024-02-18 18:47:08 -0800

    wip

--- python/chaparral/__init__.py
+++ python/chaparral/__init__.py
@@ -1,30 +1,33 @@
-import asyncio
-
-from typing import  Optional
-from grpclib.client import Channel
-
-from .gen.chaparral.v1 import AccessServiceStub, CommitServiceStub
+from typing import Optional
+from httpx import AsyncClient
 
 
-class Client(Channel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.access_service = AccessServiceStub(self)
-        self.access_service.metadata = {}
-        self.commit_service = CommitServiceStub(self)
-        self.commit_service.metadata = {}
+# routes
+access_svc_rt = "chaparral.v1.AccessService"
+get_version_rt = access_svc_rt + "/GetObjectVersion"
 
-    def set_bearer_token(self, token: str):
-        auth = "Bearer" + token
-        self.access_service.metadata["authorization"] = auth
-        self.access_service.metadata["authorization"] = auth
 
-    async def a_get_version(self, obj_id: str, root_id: str = "", version: int = 0):
-        return await self.access_service.get_object_version(storage_root_id = root_id,
-                                                            object_id = obj_id,
-                                                            version=version)
+class Client(AsyncClient):
+    def __init__(self,
+                 base_url: str = "http://127.0.0.1:8080",
+                 token: Optional[str] = None):
+        headers = {
+            "content-type": "application/json",
+            "authorization": "Bearer " + token
+        }
+        super().__init__(base_url=base_url,
+                         http2=True,
+                         headers=headers)
 
-    def get_version(self, obj_id: str, root_id: str = "", version: int = 0):
-        loop = asyncio.get_event_loop()
-        result = loop.run_until_complete(self.a_get_version(obj_id, root_id, version))
-        return result
+    async def a_get_version(self, obj: str, root: str = "", ver: int = 0):
+        body = {
+            "storageRootId": root,
+            "objectId": obj,
+            "version": ver
+        }
+        return await self.post(get_version_rt, json=body)
+
+    # def get_version(self, obj: str, root: str = "", ver: int = 0):
+    #     loop = asyncio.get_event_loop()
+    #     result = loop.run_until_complete(self.a_get_version(obj, root, ver))
+    #     return result
--- python/tests/__init__.py
+++ python/tests/__init__.py
@@ -3,17 +3,11 @@ import chaparral
 import os
 
 token = os.getenv("CHAPARRAL_TOKEN")
-host = "127.0.0.1"
-port = "8080"
+host = "http://127.0.0.1:8080"
 
-if token == "":
-    print("warning: token isn't set")
 
 async def main():
-    async with chaparral.Client(host, port) as client:    
-        client.set_bearer_token(token)
+    async with chaparral.Client(host, token) as client:
         result = await client.a_get_version("new-object-01")
-        paths = [p for k, val in result.state.items() for p in val.paths ]
-        print(paths)
+        print(result)
 asyncio.run(main())
-
